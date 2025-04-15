981af77c757625ce5cc0a9cb34610f2e562d0f7f
Cherryl <cherrylcallistacheniago@gmail.com>
2025-04-15 12:33:08 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+function App() {
+    return (
+        <div className='container'>
+            <div className="content">
+                <div className="text-div">
+                    <p className="text-20px">Kecerdasan visual</p>
+
+                    <p className="header">Lihat dunia dari lensa yang baru.</p>
+
+                    <p className="text-18px">
+                        Gunakan <span className="bold">kecerdasan visual</span> melalui Kontrol Kamera untuk langsung mempelajari tentang semua hal yang Anda liat. Cukup <span className="bold"> arahkan iPhone 16 Pro Anda untuk menemukan lebih</span> atau berinteraksi dengan sesuatu di depan Anda. Caritahu di mana tempat membeli barang baru yang Anda lihat, identifikasikan dan hewan, dan banyak lagi.
+                    </p>
+                </div>
+                <div>
+                    <img src="/phone.png" className="phone-img"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
