f15f8ceed33930313e49a9fa2e9361ef8acb46fb
Cherryl <cherrylcallistacheniago@gmail.com>
2025-04-16 11:19:41 +0700

    Previously, size avaiable: 1920px, 1280px.
    Now, added: 720px, 375px.

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@ function App() {
         <div className='container'>
             <div className="content">
                 <div className="text-div">
-                    <p className="text-20px">Kecerdasan visual</p>
+                    <p className="sub-header">Kecerdasan visual</p>
 
                     <p className="header">Lihat dunia dari lensa yang baru.</p>
 
-                    <p className="text-18px">
+                    <p className="text-pc">
                         Gunakan <span className="bold">kecerdasan visual</span> melalui <br />
                         Kontrol Kamera untuk langsung mempelajari <br />
                         tentang semua hal yang Anda liat. Cukup <br />
                         <span className="bold"> arahkan iPhone 16 Pro Anda untuk menemukan lebih</span> atau berinteraksi dengan sesuatu di depan <br /> Anda. Caritahu di mana tempat membeli barang <br />baru yang Anda lihat, identifikasikan tanaman <br />
                         dan hewan, dan banyak lagi.
+                    </p>
+
+                    <p className="text-mb">
+                        Gunakan <span className="bold">kecerdasan visual</span> melalui Kontrol Kamera <br />
+                        untuk langsung mempelajari tentang semua hal yang <br />
+                        Anda liat. Cukup <span className="bold">arahkan iPhone 16 Pro Anda <br />
+                        untuk menemukan lebih</span> atau berinteraksi dengan <br />
+                        sesuatu di depan Anda. Caritahu di mana tempat <br />
+                        membeli barang baru yang Anda lihat, identifikasikan <br />
+                        tanaman dan hewan, dan banyak lagi.
                     </p>
                 </div>
                 <div>
